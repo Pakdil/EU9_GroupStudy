62cc5095e2d73a8abeac14da78d789ec373af466
Pakdil <eaasbb@gmail.com>
2022-08-03 22:19:53 +0300

    Pakdil test case committed

--- src/Pakdil.java
+++ src/Pakdil.java
@@ -2,6 +2,11 @@ public class Pakdil {
 
     // test case pakdil started
     // test case pakdil started
-    //
+    public static void main(String[] args) {
+
+
+        System.out.println("Pakdil");
+    }
+
 
 }
